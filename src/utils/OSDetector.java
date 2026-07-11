3524d60f4612a2989ccf5e6d3958cf4d90839a6a
Andrew <voiaandrei22@yahoo.com>
2026-07-11 14:36:37 +0300

    Update OSDetector.java

--- src/utils/OSDetector.java
+++ src/utils/OSDetector.java
@@ -2,23 +2,27 @@ package utils;
 
 import java.util.Locale;
 
-
-//Detects OS
+//used to detect the current OS
 public final class OSDetector {
     
     private OSDetector() {}
     
-    //Returns OS name
-        public static String detectOS() {
+    //returns current OS name
+    public static String detectOS() {
         String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        
         if (osName.contains("win")) {
             return "Windows";
+            
         } else if (osName.contains("mac")) {
             return "macOS";
+            
         } else if (osName.contains("linux")) {
             return "Linux";
+            
         } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
             return "Unix";
+            
         } else {
             return "Unknown OS";
         }
